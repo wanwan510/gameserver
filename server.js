e80d9bc769de558b38338ea52e57355a7db76362
wanwan510 <kanwunwan@gmail.com>
2025-05-27 11:48:01 +0800

    270525

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
-/*var http = require('http');
-http.createServer(function(request, response){
-	response.write("Hello World!");
-	response.end(); //close connection
-}).listen(80); //http port
-
-console.log('Now, listening on port 8080');*/
+// var http = require('http');
+// http.createServer(function(request, response){
+// 	response.write("Hello World!");
+// 	response.end(); //close connection
+// }).listen(80); //http port
 var express = require("express");
 var server = express();
 
+var bodyParser = require("body-parser");
+
+
 server.use(express.static(__dirname+"/public"));
+server.use(bodyParser.urlencoded({extended:true}));
+server.use(bodyParser.json());
 
-/*server.get("/",function(req,res){ //web root
- 	res.send("Hello, World!"); //回傳固定內容
+ server.get("/", function (req, res) { //web root
+     res.send("Hello, World!"); //回傳固定內容
  });
- server.get("game",function(req,res){ //other pages
- 	res.send("req/md");
- });*/
- server.listen(80);
+  /*server.get("/score", function (req, res) { //other pages
+    //url?user=md&score=1000
+    console.log(req.query);
+     //res.send("req /md");
+  });*/
 
+ server.post("/postscore", (req,res)=>{
+    console.log(req.body)
 
+ });
+ 
+server.listen(80);
